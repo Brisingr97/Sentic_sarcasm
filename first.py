c7af894921e4aff1c4e07d78155a650152bf60f2
Shubham Singh Tanwar <shubhamtanwar97@gmail.com>
2016-12-27 14:16:17 +0530

    To_work_in_files

---
+++ first.py
@@ -0,0 +1,13 @@
+import random 
+import sys
+import os
+import openpyxl
+
+'''
+functions that i need --
+to open file
+then to take input from the file
+identify the characteristics i might be needing :- find valid words
+add them in record in senticnet framework, all are treated as strings.
+close the file.
+'''
